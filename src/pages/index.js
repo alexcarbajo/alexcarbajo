a6a2dfb59237a6947a10ae9fd8b69e7f9e187af5
Alex Carbajo <alexcarbajo@gmail.com>
2018-12-25 17:43:35 +0100

    First version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import Hero from '../components/Hero'
+import Hero from '../components/hero'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
