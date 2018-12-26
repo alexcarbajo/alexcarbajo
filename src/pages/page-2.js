813334d927237a333da1bd8ba12761957099334a
Alex Carbajo <alexcarbajo@gmail.com>
2018-12-26 01:10:44 +0100

    Title for Homepage

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,15 @@ import SEO from '../components/seo'
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Resume</h1>
+  
+    Born in Santiago de Compostela in 1987 and now based in Barcelona.
+    Experience
+    UX / UI Development Lead, Suntransfers  2016 - Present
+    Suntransfers.com is the largest B2C company for airport transfers to city centres.
+    Lead a team of designers and developers in design thinking, UCD, data-informed, design systems, web performance and iterative methodologies to deﬁne intuitive product experiences.
+    Data-driven design, defining hypothesis, KPIs and measure success for A/B tests in the main channels of Suntransfers.
+    Definition of components, CSS Architecture and web performance analysis and hands-on coding.
   </Layout>
 )
 
