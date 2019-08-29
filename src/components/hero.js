c0560401c5246bca6475e669e0218bde13187e2e
Alex Carbajo <alexcarbajo@gmail.com>
2019-08-29 20:09:16 +0200

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,13 +4,13 @@ class Hero extends React.Component {
   render() {
     return (
       <div className="o-Hero">
-        <h1><span className="u-Highlight">Product Designer</span> focused in travel industry and <span className="u-Nowrap">data-driven</span> design.</h1>
+        <h1><span className="u-Highlight">Product Designer</span> specialized in Design Systems, Design Strategy and Product Management.</h1>
         <h2>
-          Hi, I'm Alex and I'm currently working at <a href="https://www.suntransfers.com" target="_blank" rel="noopener noreferrer">Suntransfers.com</a> <span role="img" aria-label="Taxi in movement" className="u-Nowrap">🚕💨</span>, leading the UX and UI Development team.
+          Hi, I'm Alex and I'm currently working at <a href="https://www.crealogix.com" target="_blank" rel="noopener noreferrer">Suntransfers.com</a> <span role="img" aria-label="Taxi in movement" className="u-Nowrap">💸</span>, leading the UX and UI Development team.
         </h2>
         
         <div class="o-Hero__menu">
-          <a href="https://drive.google.com/open?id=1K9mZ6EUh6TqICtcslGFnLwZMKPnptBfu" target="_blank" rel="noopener noreferrer" className="c-Button c-Button--primary c-Button--margin-right">Resume</a>
+          <!--<a href="https://drive.google.com/open?id=1K9mZ6EUh6TqICtcslGFnLwZMKPnptBfu" target="_blank" rel="noopener noreferrer" className="c-Button c-Button--primary c-Button--margin-right">Resume</a>-->
           <a className="c-Link c-Link--highlight" href="mailto:alexcarbajo@gmail.com">Contact</a>
         </div>
       </div>
